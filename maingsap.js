d4e1eaba5418d90c8540b4fcec2d2395a0742b6b
Arjun_2025 <vineetaarjun123@gmail.com>
2026-03-24 15:36:40 +0530

    added navbar ui

---
+++ maingsap.js
@@ -0,0 +1,35 @@
+// for mainpage
+
+//main-logo
+gsap.from(".main-logo",{
+    x : -300,
+    duration :2,
+    ease : "power1.out"
+})
+
+gsap.from(".github-logo",{
+    x : 300,
+    duration :2,
+    ease : "power1.out"
+})
+
+gsap.from(".main-head",{
+    y : 300,
+    duration :2,
+    ease : "power1.out"
+})
+
+gsap.from(".container",{
+    x : -300,
+    y  : -300,
+    duration :2,
+    ease : "power1.out"
+})
+
+
+gsap.from(".navbar",{
+   
+    y  : -300,
+    duration :2,
+    ease : "power1.out"
+})
